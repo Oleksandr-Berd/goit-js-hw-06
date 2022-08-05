56e8bb75cc74a70c692ac292ee25eca321a1d7ca
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-05 16:01:47 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,21 @@
-console.dir(document);
-
-const categRef = document.querySelector('#categories');
-
-const categItemRef = categRef.querySelectorAll('.item');
+const categItemRef = document.querySelectorAll("ul#categories li.item");
 
 console.log(categItemRef);
 
 const numOfCateg = categItemRef.length;
 
-console.log('Number of categories: ', numOfCateg);
+console.log("Number of categories: ", numOfCateg);
+
+categItemRef.forEach((element) => {
+  const elemRef = element;
+  // console.log(elemRef);
+  const elemRefTitle = elemRef.firstElementChild;
+
+  const textTitle = elemRefTitle.textContent;
+
+  console.log("Category: ", textTitle);
+
+  const test = elemRef.querySelectorAll("li").length;
+
+  console.log("Elements: ", test);
+});
