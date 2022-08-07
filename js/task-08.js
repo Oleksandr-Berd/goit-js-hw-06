777137d8fb5a9338ae6327946da8cb4541d0bfbd
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-07 19:43:32 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email;
+
+  const password = formElements.password;
+
+  const newObj = { mail, password };
+
+  mail.value === "" || password.value === ""
+    ? alert("Warning All fields have to be filled!")
+    : (newObj.mail = mail.value),
+    (newObj.password = password.value);
+  console.log(newObj);
+  form.reset();
+}
