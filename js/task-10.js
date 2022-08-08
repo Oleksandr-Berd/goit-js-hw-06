6d37d4a93274a24aa4c6e25c7587b3724e178f2d
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-08 20:35:07 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnCreateRef = document.querySelector("button[data-create]");
+const btnDestRef = document.querySelector("button[data-destroy]");
+const inputRef = document.querySelector("#controls > input");
+const boxRef = document.querySelector("#boxes");
+
+let amount = 0;
+
+function createAmount(event) {
+  amount = Number(event.currentTarget.value);
+  return amount;
+}
+
+function launchEvent(event) {
+  let i = 0;
+  let width = 10;
+  let height = 10;
+
+  do {
+    i += 1;
+    width += 10;
+    height += 10;
+    const newDiv = document.createElement("div");
+    newDiv.style.width = `${width}px`;
+    newDiv.style.height = `${height}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+    boxRef.append(newDiv);
+    console.log(boxRef);
+  } while (i < amount);
+}
+
+inputRef.addEventListener("input", createAmount);
+
+btnCreateRef.addEventListener("click", launchEvent);
